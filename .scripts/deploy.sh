8bccfd2b52cddb7de5e1a6162ee5561916f4bb5d
Damian F <damian.fikier@wp.pl>
2019-11-17 23:10:12 +0100

    Fixing env variables issues.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,6 +8,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   ssh -v $DROPLET_USER@$DROPLET_IP -o StrictHostKeyChecking=no <<-EOF
     cd $SSH_PROJECT_FOLDER
     git pull
+    echo "export POSTGRES_DB=$POSTGRES_DB"
+    echo "export POSTGRES_USER=$POSTGRES_USER"
+    echo "export POSTGRES_PWD=$POSTGRES_PWD"
+    echo "export PGADMIN_PORT=$PGADMIN_PORT"
+    echo "export PGADMIN_LOGIN=$PGADMIN_LOGIN"
+    echo "export PGADMIN_PWD=$PGADMIN_PWD"
+    echo "export JWT_SECRET_KEY=$JWT_SECRET_KEY"
+    echo "export POSTGRES_PORT=5432"
     docker-compose pull
     docker-compose stop
     docker-compose rm -f
