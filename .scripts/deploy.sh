c101507e08ee3c8f97697ae89537fdc2fe6379ee
Damian F <damian.fikier@wp.pl>
2019-11-17 22:15:55 +0100

    Env variables fix

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,12 +6,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   cd "$DIR/.." # Go to project dir.
 
   ssh -v $DROPLET_USER@$DROPLET_IP -o StrictHostKeyChecking=no <<-EOF
-    export POSTGRES_DB=$POSTGRES_DB
-    export POSTGRES_USER=$POSTGRES_USER
-    export POSTGRES_PWD=$POSTGRES_PWD
-    export PGADMIN_PORT=$PGADMIN_PORT
-    export PGADMIN_LOGIN=$PGADMIN_LOGIN
-    export PGADMIN_PWD=$PGADMIN_PWD
     cd $SSH_PROJECT_FOLDER
     git pull
     docker-compose pull
