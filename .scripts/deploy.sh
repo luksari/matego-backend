61ab2a240a4555b76f4bac36f1794b1c158bdd0f
Damian F <damian.fikier@wp.pl>
2019-11-17 22:15:55 +0100

    Env variables fix

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,14 +6,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   cd "$DIR/.." # Go to project dir.
 
   ssh -v $DROPLET_USER@$DROPLET_IP -o StrictHostKeyChecking=no <<-EOF
-    export POSTGRES_DB=$POSTGRES_DB
-    export POSTGRES_USER=$POSTGRES_USER
-    export POSTGRES_PWD=$POSTGRES_PWD
-    export PGADMIN_PORT=$PGADMIN_PORT
-    export PGADMIN_LOGIN=$PGADMIN_LOGIN
-    export PGADMIN_PWD=$PGADMIN_PWD
     cd $SSH_PROJECT_FOLDER
     git pull
+    echo "export POSTGRES_DB=$POSTGRES_DB"
+    echo "export POSTGRES_USER=$POSTGRES_USER"
+    echo "export POSTGRES_PWD=$POSTGRES_PWD"
+    echo "export PGADMIN_PORT=$PGADMIN_PORT"
+    echo "export PGADMIN_LOGIN=$PGADMIN_LOGIN"
+    echo "export PGADMIN_PWD=$PGADMIN_PWD"
+    echo "export JWT_SECRET_KEY=$JWT_SECRET_KEY"
+    echo "export POSTGRES_PORT=5432"
+    echo "export POSTGRES_HOST=postgres"
     docker-compose pull
     docker-compose stop
     docker-compose rm -f
--- src/config/typeorm.config.service.ts
+++ src/config/typeorm.config.service.ts
@@ -6,11 +6,11 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: process.env.POSTGRES_HOST_DEV,
-      port: parseInt(process.env.POSTGRES_PORT_DEV, 10),
-      username: process.env.POSTGRES_USERNAME_DEV,
-      password: process.env.POSTGRES_PASSWORD_DEV,
-      database: process.env.POSTGRES_DATABASE_DEV,
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PWD,
+      database: process.env.POSTGRES_DB,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
     };
