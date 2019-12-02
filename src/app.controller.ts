b6b29f534f118dac7d0c6ae9a7789b1a5cff4e29
Damian F <damian.fikier@wp.pl>
2019-12-02 22:53:38 +0100

    Unused imports fix.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { rpc } from 'protobufjs';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from './auth/guards/roles.guard';
 import { Roles } from './decorators/roles.decorator';
