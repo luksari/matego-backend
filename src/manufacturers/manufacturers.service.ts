ea7ef41463ec41f3567fb1ce8db289d641e8e86f
Łukasz Tyszkiewicz <lukasz.tyszkiewicz@leocode.com>
2019-12-21 16:44:00 +0000

    Added sorting in Manufacturers table

--- src/manufacturers/manufacturers.service.ts
+++ src/manufacturers/manufacturers.service.ts
@@ -7,23 +7,25 @@ import { User } from '../users/user.entity';
 import { EditManufacturerInput } from './edit.manufacturer.input';
 import { ErrorMessages } from '../common/error.messages';
 import { ManufacturersResponse } from './manufacturers.response';
+import { Order } from 'src/common/enums';
 
 @Injectable()
 export class ManufacturersService {
   constructor(
     @InjectRepository(Manufacturer)
     private readonly manufacturersRepository: Repository<Manufacturer>,
-    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    @InjectRepository(User) 
+    private readonly usersRepository: Repository<User>,
   ) {}
 
-  async getAll(_offset: number, _limit: number): Promise<ManufacturersResponse> {
-    const offset = _offset || 0;
-    const limit = _limit || 15;
-    const [items, total] = await this.manufacturersRepository.findAndCount({
-      skip: offset,
-      take: limit,
-      relations: ['products'],
-    });
+  async getAll(offset: number = 0, limit: number = 15, orderBy: string = 'id', order: Order = Order.ASC): Promise<ManufacturersResponse> {
+    const [items, total] = await this.manufacturersRepository
+    .createQueryBuilder(Manufacturer.name)
+    .leftJoinAndSelect(`${Manufacturer.name}.products`, 'products')
+    .orderBy(`${Manufacturer.name}.${orderBy}`, order)
+    .skip(offset)
+    .take(limit)
+    .getManyAndCount();
     return { items, total }
   }
   async findById(id: number): Promise<Manufacturer> {
